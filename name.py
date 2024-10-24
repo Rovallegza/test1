2aa9368c635efff8b28fbee1b4902d1b7d030634
Adelfo <adelfodangf@gmail.com>
2024-10-24 16:02:02 -0600

    tu codigo corregido brou :D

--- name.py
+++ name.py
@@ -1,3 +1,3 @@
 name = input ("Escribe tu nombre carnalito: ")
-last_name = inputt ("Escribe tu apellido carnalito: ")
+last_name = input ("Escribe tu apellido carnalito: ")
 print (f"¡Hola {name} {last_name}!")
